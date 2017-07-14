ccb61097f2539d7c330752f85b175b2b74ca0c7c
Steven Chun <steven.r.chun.19@dartmouth.edu>
2017-07-14 10:42:13 -0400

    change load order

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-particlesJS.load('particles-js', 'js/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
-$(document).ready(function() {
+$(window).load(function() {
   $(".hello").addClass("visible animated fadeInDown");
   $(".subtitle").addClass("visible animated fadeInDown");
   $(".tiny").addClass("visible animated fadeInDown");
+
+  particlesJS.load('particles-js', 'js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
 });
