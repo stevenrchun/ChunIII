f6fafcefb394b3e67c1e8e16448625730016179a
Steven Chun <steven.r.chun.19@dartmouth.edu>
2017-09-18 03:59:59 -0400

    switch out assets, add gradient babackground, add new sections

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@ $(window).load(function() {
   $(".hello").addClass("visible animated fadeInDown");
   $(".subtitle").addClass("visible animated fadeInDown");
   $(".tiny").addClass("visible animated fadeInDown");
-
   particlesJS.load('particles-js', 'js/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
-
 });
